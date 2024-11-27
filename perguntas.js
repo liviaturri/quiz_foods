71feea9b0279d6f7bdd8f506e391b809e390a947
liviaturri <168583432+liviaturri@users.noreply.github.com>
2024-11-26 22:18:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Culinária Internacional',
+    'A pizza moderna foi popularizada na cidade de Nápoles, mas qual ingrediente essencial só foi incorporado ao prato após ser trazido das Américas?',
+    'Tomate'
+)
+
+criaCartao(
+    'Ingredientes',
+    'O abacate, base do guacamole, é valorizado por seu sabor neutro e textura cremosa. Por que essas características o tornam tão versátil na culinária?',
+    'Porque se adapta a diferentes temperos e receitas.'
+)
+
+criaCartao(
+    'Pratos Típicos',
+    'O sushi moderno surgiu no Japão, mas originalmente, o método “narezushi” era usado para conservar peixe. Que ingrediente desse método fermentava o peixe?',
+    'Arroz'
+)
+
+criaCartao(
+    'Doces e Sobremesas',
+    'O pé-de-moleque, típico no Brasil, é feito com amendoim e açúcar. Em qual período histórico ele se tornou popular como doce festivo?',
+    'Período colonial'
+)
+
+criaCartao(
+    'Frutas',
+    'A manga é conhecida como “rainha das frutas”. Em qual país ela é considerada símbolo nacional e cultivada há mais de 4 mil anos?',
+    'Índia'
+)
+
+criaCartao(
+    'Panificação',
+    'Qual é o ingrediente indispensável usado para fazer pães, proporcionando sua estrutura e textura macia?',
+    'Farinha de trigo'
+)
+
+criaCartao(
+    'Culinária Global',
+    'O famoso molho "sriracha", que conquistou paladares em todo o mundo, tem origens na Tailândia. Qual ingrediente essencial, que confere seu sabor picante, é cultivado há mais de 6.000 anos na Ásia?',
+    'Pimenta'
+)
+
+criaCartao(
+    'Cultura Gastronômica',
+    'O chocolate, um dos doces mais amados no mundo, tem uma origem ancestral na civilização maia, onde era considerado uma bebida sagrada. Qual era o nome dessa bebida ritualística feita a partir do cacau?',
+    'Xocoatl'
+)
+
+criaCartao(
+    'Alimentos Inusitados',
+    'A carne de insetos, uma prática culinária que remonta a séculos, está ganhando popularidade como uma solução sustentável. Qual é o principal benefício ambiental dessa proteína alternativa, comparado à carne tradicional?',
+    'Menor impacto ambiental e uso de menos recursos naturais'
+)
